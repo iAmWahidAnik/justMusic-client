560d4dd726dca4c84a8e7182290942f2fc40054b
wahidanik <wahid.anik@outlook.com>
2023-06-07 23:35:50 +0600

    credentials added to environment variables

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAH8l8B7cA1F2UClWLKU5rOgOkmliiC3Ro",
-  authDomain: "justmusic-e7600.firebaseapp.com",
-  projectId: "justmusic-e7600",
-  storageBucket: "justmusic-e7600.appspot.com",
-  messagingSenderId: "960892645288",
-  appId: "1:960892645288:web:5c3062ad8b354edb8cf79d"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
